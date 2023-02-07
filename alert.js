ed52e3401faa6ab943e358fd225a48fdeff64c32
Vlad <efremov.vlad.ser@gmail.com>
2023-02-07 15:09:19 +0300

    feat: add rating by number of films

--- alert.js
+++ alert.js
@@ -16,9 +16,21 @@ for (let i = 0; i < 2; i++) {
         
   if(a != null && b != null && a != '' && b != '' && a.length < 50) {
     personalMovieDB.movies[a] = b;
+    console.log('done');
   } else {
+    console.log('error');
     i--;
   }
+}
+
+if (personalMovieDB.count < 10) {
+  console.log("Просмотрено довольно мало фильмов");
+} else if( personalMovieDB.count >= 10 && personalMovieDB.count < 30 ) {
+  console.log( "Вы классический зритель" );
+} else if( personalMovieDB.count >= 30 ) {
+  console.log( "Вы киноман" );
+} else {
+  console.log( "Произошла ошибка" );
 }
 
 console.log(personalMovieDB);
