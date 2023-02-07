fef74da76e43d4cbbb149edb7c7bc54325fa4c2a
Vlad <efremov.vlad.ser@gmail.com>
2023-02-07 14:55:04 +0300

    feat: add check for empty and long string

--- alert.js
+++ alert.js
@@ -10,11 +10,15 @@ const personalMovieDB = {
   privat: false
 };
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
 for (let i = 0; i < 2; i++) {
   const a = prompt('Один из последних просмотренных фильмов?', ''),
-        b = prompt('На сколько оцените его?', ''),
+        b = prompt('На сколько оцените его?', '');
+        
+  if(a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+  } else {
+    i--;
+  }
 }
+
 console.log(personalMovieDB);
